10328c8a2150f9e55dc74c1a085e7988b69f1116
HelloCho <skidrow0802@gmail.com>
2020-10-15 00:02:38 +0900

    1:1 대화방 구축 현재 나의 셀, 상대방 셀 구현

---
+++ kakaotalk_starter/YourCell.swift
@@ -0,0 +1,24 @@
+//
+//  YourCell.swift
+//  kakaotalk_starter
+//
+//  Created by home on 2020/10/14.
+//  Copyright © 2020 James Kim. All rights reserved.
+//
+
+import UIKit
+
+class YourCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
